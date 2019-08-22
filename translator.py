1f12d615cfbd0228e8923d9d8eb2ed442e10a43a
Athar Bakth <atharbakth@gmail.com>
2019-08-22 17:33:49 -0400

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,10 +1,9 @@
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '0','1', '2', '3', '4','5', '6', '7', '8', '9', '.', ',' ,'?', ';', ':', '/', '-', '\'', '(', ')', '_', '@', '!', '&', '=', '+', '"', '$']
 morse_alphabet =['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', ' ', '-----','.----', '..---', '...--', '....-','.....', '-.....', '--...', '---..', '----.', '.-.-.-', '--..--' ,'..--..', '-.-.-', '---...', '-..-.', '-....-', '.----.', '-.--.', '-.--.-', '..--.-', '.--.-.', '-.-.--', '.-...', '-...-', '.-.-.', '.-..-.', '...-..-']
 
-sentence = str(input('What would you like to say? '))
-upper_sentence = list(sentence.upper())
 
-def translator(list_sentence):
+def translator(sentence):
+    list_sentence = list(sentence.upper())
     indexes = []
     illegal_characters = []
     count = -1
@@ -15,7 +14,7 @@ def translator(list_sentence):
             if letters not in alphabet:
                 count += 1
                 illegal_characters.append(letters)
-                print('That cannot be converted '+ illegal_characters[count])
+                #print('That cannot be converted '+ illegal_characters[count])
             
     morse_sentence = []
     for numbers in indexes:
@@ -24,4 +23,6 @@ def translator(list_sentence):
     translated = ' '.join(morse_sentence)
     print(translated)
 
-translator(upper_sentence)
+while True:
+    new_sentence = str(raw_input('what would you like to translate? '))
+    translator(new_sentence)
